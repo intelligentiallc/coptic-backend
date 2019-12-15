9814ffe2c96bd943246073f954042e701eef09fb
intelligentiallc <intelligentiallc@gmail.com>
2019-12-15 15:32:42 -0500

    add mongodb host

--- database/config.js
+++ database/config.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/coptic", { useNewUrlParser: true , useUnifiedTopology: true });
+if (process.env.NODE_ENV === "development") {
+  mongoose.connect("mongodb://localhost/coptic", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+} else {
+  mongoose.connect(
+    "mongodb+srv://mina:123@coptic-snvzk.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+}
 const db = mongoose.connection;
 if (!db) console.log("Error connecting db");
 else console.log("Db connected successfully");
